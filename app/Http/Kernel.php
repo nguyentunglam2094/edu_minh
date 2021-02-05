df4775bdf705baada1164d816cc1bbbacce93a1c
nguyentunglam2094 <ltnguyen.tl@gmail.com>
2021-02-06 02:14:01 +0700

    pussh

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'logged' => \App\Http\Middleware\CheckLogged::class,
+        'logged' => \App\Http\Middleware\checkLogged::class,
     ];
 }
